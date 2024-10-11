183c8da5c24ddc611404b61a786652f2fddf5c32
Iván Domonkos <ivandomonkos@edu.bme.hu>
2024-10-11 22:02:44 +0200

    new project page added

---
+++ apps/frontend/src/app/newproject/page.tsx
@@ -0,0 +1,9 @@
+import SprintLogin from '../../components/NewProject';
+
+export default function Home() {
+  return (
+    <main className='flex items-center justify-center bg-page-bg-color'>
+      <SprintLogin />
+    </main>
+  );
+}
